14ab74c7cb3d080d1e7115120c025925ae9aace8
amrullahamiri12 <amrullah.amiri12@gmail.com>
2024-07-11 13:18:05 -0700

    login

---
+++ src/test/java/com/automation/stepDefinitions/LoginStepDefs.java
@@ -0,0 +1,10 @@
+package com.automation.stepDefinitions;
+
+import io.cucumber.java.en.When;
+
+public class LoginStepDefs {
+    @When("I enter the correct email address and password")
+    public void i_enter_the_correct_email_address_and_password() {
+
+    }
+}
